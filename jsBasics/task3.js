263c4307df874ae1439534f6190985d45a348281
denys.zagrebelnyi <denys.zagrebelnyi@mgid.com>
2024-05-19 17:00:46 +0300

    Added the homeWork-5 folder with the files. Changed the “checkingAge” variable to “isAdult” in jsBasics/task3.js

--- jsBasics/task3.js
+++ jsBasics/task3.js
@@ -1,4 +1,4 @@
 let age = 23
-let checkingAge = age > 18 - 1
+let isAdult = age > 18 - 1
 console.log(`Вік персони: ${age}`)
-console.log(`Чи особа повнолітня: ${checkingAge}`)
+console.log(`Чи особа повнолітня: ${isAdult}`)
