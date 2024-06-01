32fe7e7cc82479aa3f92ed32387f6ded06d0b426
denys.zagrebelnyi <denys.zagrebelnyi@mgid.com>
2024-06-01 11:25:59 +0300

    Added the homeWork-9 folder with the file

---
+++ homeWork-9/task1.js
@@ -0,0 +1,6 @@
+const student = {}
+student.firstName = `Denys`
+student.lastName = `Zagrebelnyi`
+student.age = 27
+student.courses = ["Математика", "Історія", "Програмування"]
+console.log(student)
